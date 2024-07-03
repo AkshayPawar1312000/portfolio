5f52e78ef7a301711f75fbcd1bd9904f8938dcab
akshay <akshay@hoppysoft.com>
2024-07-03 17:37:26 +0530

    .

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 // ============================ typibg Animation ====================
 var typed = new Typed(".typing", {
-    strings: ["","Web Designer","web Devloper", "Full Stack Devloper", "Software Engineer"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop: true
-})
+  strings: [
+    "",
+    "Full Stack Devloper",
+    "MERN Stack Devloper",
+    "web Devloper",
+    "Software Engineer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
